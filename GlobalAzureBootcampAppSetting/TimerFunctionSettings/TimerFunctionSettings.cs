6e02ec54bea787a775fba10735019e94c3d72d28
Andrea Tosato <andrea.tosato@hotmail.it>
2018-04-20 15:10:22 +0200

    Rimozione Function Annotation

--- GlobalAzureBootcampAppSetting/TimerFunctionSettings/TimerFunctionSettings.cs
+++ GlobalAzureBootcampAppSetting/TimerFunctionSettings/TimerFunctionSettings.cs
@@ -6,7 +6,6 @@ namespace GlobalAzureBootcampAppSetting
 {
     public static class TimerFunctionSettings
     {
-        [FunctionName("TimerFunctionSettings")]
         public static void Run(TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"TimerFunctionSettings trigger function executed at: {DateTime.Now}");
